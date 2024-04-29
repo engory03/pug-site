b5603d152f76ce91953de3140665eae12be46ace
Liz <eakorobova2005@mail.ru>
2024-04-29 21:24:02 +0300

    Refactor code

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 const form = document.getElementById("order-form");
-const nameInput = document.getElementById("name");
-const phoneInput = document.getElementById("phone");
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav");
 
+// Burger menu
 burger.addEventListener("click", () => {
   nav.classList.toggle("active");
   burger.classList.toggle("active");
 });
 
+// Form
 function submitForm(event) {
   event.preventDefault();
-  //   console.log(event);
-  //   console.log(event.target);
 
-  //   for (var [key, value] of data.entries()) {
-  //     console.log(key, value);
-  //   }
+  const formData = new FormData(form);
+  let data = {};
 
-  const data = new FormData();
-  data.append("name", nameInput.value);
-  data.append("phone", phoneInput.value);
-  console.log(nameInput.value, phoneInput.value);
+  for (var [key, value] of formData.entries()) {
+    console.log(key, value);
+    data[key] = value;
+  }
 
   fetch("https://my-own-library.vercel.app/api/auth/signin", {
     method: "POST",
-    body: JSON.stringify({ name: nameInput.value, phone: phoneInput.value }),
+
+    // JSON data
+    body: JSON.stringify(data),
+
+    // form data
+    // body: formData,
   });
 }
 
